5f0138af1e3acc3aed94e890354de8446081923d
bsougane <bsougane@gmail.com>
2020-09-08 13:25:39 +0000

    first

---
+++ src/test/java/com/souga/sgsp/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souga.sgsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
